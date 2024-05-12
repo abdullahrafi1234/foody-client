1c12c7f065bd780061b1fc53d6cf778a71bb420c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-05-12 12:47:13 +0600

    added footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,10 +1,50 @@
+import { Link } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaGithub, FaInstagram, } from "react-icons/fa";
 
 
 const Footer = () => {
     return (
-        <div>
-            <h3>footer here</h3>
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content grid ">
+            <aside className="flex flex-col">
+                <div className="flex items-center gap-3 ">
+                    <img className="w-16" src='logo food.png' alt="" />
+                    <div className=" workSans text-3xl text-green-800 font-bold gap-0">
+                        <Link to={'/'} className="gap-0 items-center">
+                            F<span className="text-red-500">OO</span>DY
+                        </Link>
+                    </div>
+                </div>
+                <p className="font-semibold">Simple Way of Eating Delicious</p>
+                <nav className=" pb-8 pt-4 ">
+                    <div className="grid grid-flow-col gap-4">
+                        <a href="https://www.facebook.com"><FaFacebook className="text-3xl text-blue-700"></FaFacebook></a>
+                        <a href="https://twitter.com"><FaTwitter className="text-3xl text-sky-400"></FaTwitter></a>
+                        <a href="https://github.com/abdullahrafi1234"><FaGithub className="text-3xl"></FaGithub></a>
+                        <a href="https://www.instagram.com"><FaInstagram className="text-3xl text-red-400"></FaInstagram></a>
+                    </div>
+                </nav>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Our Services</h6>
+                <a className="link link-hover">Pricing</a>
+                <a className="link link-hover">Tracking</a>
+                <a className="link link-hover">Report a Bug</a>
+                <a className="link link-hover">Terms of Services</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Our Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Management</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Address</h6>
+                <a className="link link-hover">Mohammadpur, Dhaka</a>
+                <a className="link link-hover">(+88) 01722438145</a>
+                <a className="link link-hover">rafibhuiyan1234@gmail.com</a>
+            </nav>
+        </footer>
     );
 };
 
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -47,28 +47,28 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar lg:px-8 rounded-b-lg shadow-lg text-blue-700 py-6">
+        <div className="navbar lg:px-8 rounded-b-lg shadow-lg  py-6">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
                         {navLinks}
                     </ul>
                 </div>
                 <div className="flex items-center gap-3 justify-center">
-                    <p><img className="w-10" src="logo food.jpg" alt="" /></p>
+                    <p><img className="w-10" src="logo food.png" alt="" /></p>
 
-                    <div className=" workSans text-3xl text-blue-900 font-bold gap-0">
-                        <Link className="gap-0 items-center">
-                            F<span className="text-red-500">oo</span>dy
+                    <div className=" workSans text-3xl text-green-800 font-bold gap-0">
+                        <Link to={'/'} className="gap-0 items-center">
+                            F<span className="text-red-500">OO</span>DY
                         </Link>
                     </div>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-semibold">
                     {navLinks}
                 </ul>
             </div>
